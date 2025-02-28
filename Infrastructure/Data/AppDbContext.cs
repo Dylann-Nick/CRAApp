55fd7d5cb113959a3a6cc8e4661c44559f009529
NickGitDev <mongoetou@gmail.com>
2025-02-28 12:23:01 +0100

    Ajout du context(AppDbContext)

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
+        public DbSet<CompteRendu> CompteRendus { get; set; }
+        public DbSet<EvaluationTuteur> EvaluationsTuteurs { get; set; }
+        public DbSet<FicheEtudiant> FichesEtudiants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Relation Utilisateur → CompteRendus
+            modelBuilder.Entity<CompteRendu>()
+                .HasOne(c => c.Etudiant)
+                .WithMany(u => u.CompteRendus)
+                .HasForeignKey(c => c.EtudiantId);
+
+            // Relation EvaluationTuteur → CompteRendu
+            modelBuilder.Entity<EvaluationTuteur>()
+                .HasOne(e => e.CompteRendu)
+                .WithOne()
+                .HasForeignKey<EvaluationTuteur>(e => e.CompteRenduId);
+
+            // Relation EvaluationTuteur → Tuteur
+            modelBuilder.Entity<EvaluationTuteur>()
+                .HasOne(e => e.Tuteur)
+                .WithMany()
+                .HasForeignKey(e => e.TuteurId);
+
+            // Relation Utilisateur → FicheEtudiant (1:1)
+            modelBuilder.Entity<FicheEtudiant>()
+                .HasOne(f => f.Etudiant)
+                .WithOne()
+                .HasForeignKey<FicheEtudiant>(f => f.EtudiantId);
+        }
+    }
+}
+
--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179a781c67f3c692c906450ef5f95692a3c085ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+941c7d64fe1a641e3d02b0c520b9642cbea322e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
